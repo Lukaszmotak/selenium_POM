53c050b39f6c525b7b026aa9984affb0f4d21674
Lukaszmotak <lukaszmotak19@gmail.com>
2026-03-15 10:46:19 +0100

    new directory - utils and test_data added

---
+++ test_data/registration_data_generator.py
@@ -0,0 +1,13 @@
+from faker import Faker
+from utils.custom_types import Gender
+
+
+class RegistrationDataGenerator:
+    def __init__(self):
+        self.fake = Faker()
+        self.FIRST_NAME = self.fake.first_name()
+
+data = RegistrationDataGenerator()
+print(data.FIRST_NAME)
+
+    pass
